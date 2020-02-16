9e139b992540734b2a8319e5f59ff56957e849f9
Manoj Kumar <Manoj.Kumar41@T-Mobile.com>
2020-02-16 22:00:11 +0530

    onChanges chart display implementation

--- libs/shared/ui/chart/src/lib/chart/chart.component.ts
+++ libs/shared/ui/chart/src/lib/chart/chart.component.ts
@@ -3,19 +3,20 @@ import {
   ChangeDetectorRef,
   Component,
   Input,
-  OnInit
+  OnInit,
+  OnDestroy
 } from '@angular/core';
-import { Observable } from 'rxjs';
-
+import { Observable, Subject } from 'rxjs';
+import { takeWhile, takeUntil } from 'rxjs/operators';
 @Component({
   selector: 'coding-challenge-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css']
 })
-export class ChartComponent implements OnInit {
+export class ChartComponent implements OnInit, OnDestroy {
   @Input() data$: Observable<any>;
   chartData: any;
-
+  private unsubscribe$: Subject<void> = new Subject<void>();
   chart: {
     title: string;
     type: string;
@@ -34,6 +35,12 @@ export class ChartComponent implements OnInit {
       options: { title: `Stock price`, width: '600', height: '400' }
     };
 
-    this.data$.subscribe(newData => (this.chartData = newData));
+    this.data$.pipe(takeUntil(this.unsubscribe$)).
+    subscribe(newData => (this.chartData = newData));
+  }
+
+  public ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
