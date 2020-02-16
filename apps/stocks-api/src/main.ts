0f05038bda9069ff0f3293ce5db168301b8f47f0
Manoj Kumar <Manoj.Kumar41@T-Mobile.com>
2020-02-16 15:25:07 +0530

    Proxy and Cache implementation

--- apps/stocks-api/src/main.ts
+++ apps/stocks-api/src/main.ts
@@ -3,20 +3,69 @@
  * This is only a minimal backend to get started.
  **/
 import { Server } from 'hapi';
+import { environment } from './environments/environment';
+const H2o2 = require('@hapi/h2o2');
 
 const init = async () => {
   const server = new Server({
     port: 3333,
-    host: 'localhost'
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ["*"],
+        headers: ["Accept", "Content-Type"]
+      }
+    }
+  });
+
+  await server.register(H2o2);
+
+  const fetchQueryData = async (symbol, period) => {
+    let result = '';
+    await server.inject(`/proxy/stock/${symbol}/chart/${period}`).then(res => {
+      result = res.payload;
+    });
+    return result;
+  };
+
+  server.method({
+    name: 'fetchQueryData',
+    method: fetchQueryData,
+    options: {
+      cache: {
+        expiresIn: 5 * 60 * 1000,
+        generateTimeout: 3000
+      }
+    }
   });
 
   server.route({
     method: 'GET',
-    path: '/',
+    path: '/beta/stock/{symbol}/chart/{period}',
+    handler: async (request, h) => {
+      return server.methods.fetchQueryData(request.params.symbol, request.params.period);
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/proxy/stock/{symbol}/chart/{period}',
+    options: {
+      handler: {
+        proxy: {
+          uri: environment.apiURL + '/beta/stock/{symbol}/chart/{period}?token=' + environment.apiKey,
+          passThrough: true,
+          xforward: true
+        }
+      }
+    }
+  });
+
+  server.route({
+    method: '*',
+    path: '/{any*}',
     handler: (request, h) => {
-      return {
-        hello: 'world'
-      };
+      return '404 Error! Page Not Found!';
     }
   });
 
